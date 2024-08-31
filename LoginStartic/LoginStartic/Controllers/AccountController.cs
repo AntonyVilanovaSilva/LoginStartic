07e58112fc11829573ee821d6b32bcb3c7845006
Antony Vilanova <115712032+AntonyVilanovaSilva@users.noreply.github.com>
2024-08-31 10:48:39 -0300

    31/08

---
+++ LoginStartic/LoginStartic/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginStartic.Controllers
+{
+    public class AccountController : Controller
+    {
+        private const string StaticUsername = "Leonardo";
+        private const string StaticPassword = "1234";
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == StaticUsername && password == StaticPassword)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Error = "Usuário ou Senha inválidos!";
+            return View();
+        }
+
+    }
+}
